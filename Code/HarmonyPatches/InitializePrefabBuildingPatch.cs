8f56e2caca4c5a64921f6a8acd34a511d3bccc8f
Tal <talrofeh@gmail.com>
2023-01-12 15:23:02 +0200

    fix naming update

--- Code/HarmonyPatches/InitializePrefabBuildingPatch.cs
+++ Code/HarmonyPatches/InitializePrefabBuildingPatch.cs
@@ -21,7 +21,7 @@ namespace IndustriesMeetsSunsetHarbor.HarmonyPatches
                     var newAI = (PrefabAI)__instance.gameObject.AddComponent<ResourceMarketAI>();
                     PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
                 }
-                else if (__instance.m_class.m_service == ItemClass.Service.Fishing && (__instance.name.Contains("Aqua Crop Extractor") || __instance.name.Contains("Aqua Fish Extractor")) && __instance.GetAI() is not AquacultureExtractorAI)
+                else if (__instance.m_class.m_service == ItemClass.Service.Fishing && (__instance.name.Contains("Aqua Crops Extractor") || __instance.name.Contains("Aqua Fish Extractor")) && __instance.GetAI() is not AquacultureExtractorAI)
                 {
                     var oldAI = __instance.GetComponent<PrefabAI>();
                     Object.DestroyImmediate(oldAI);
