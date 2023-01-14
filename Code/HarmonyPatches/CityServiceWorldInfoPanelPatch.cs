94175d7fe492e94f46c2631c74f9ba9444f55c01
Tal <talrofeh@gmail.com>
2023-01-15 01:01:15 +0200

    support for output resource

--- Code/HarmonyPatches/CityServiceWorldInfoPanelPatch.cs
+++ Code/HarmonyPatches/CityServiceWorldInfoPanelPatch.cs
@@ -3,6 +3,8 @@ using ColossalFramework.UI;
 using HarmonyLib;
 using System.Reflection;
 using IndustriesMeetsSunsetHarbor.UI;
+using IndustriesMeetsSunsetHarbor.AI;
+using ColossalFramework;
 
 namespace IndustriesMeetsSunsetHarbor.HarmonyPatches
 {
@@ -11,22 +13,23 @@ namespace IndustriesMeetsSunsetHarbor.HarmonyPatches
     public static class CityServiceWorldInfoPanelPatch
     {
         [HarmonyPostfix]
-        public static void Postfix(CityServiceWorldInfoPanel __instance)
+        public static void Postfix(CityServiceWorldInfoPanel __instance, Building data)
         {
-            var m_fishFarmAI = (FishFarmAI)typeof(CityServiceWorldInfoPanel).GetField("m_fishFarmAI", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
             var m_outputBuffer = (UIProgressBar)typeof(CityServiceWorldInfoPanel).GetField("m_outputBuffer", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
             var m_outputLabel = (UILabel)typeof(CityServiceWorldInfoPanel).GetField("m_outputLabel", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
             var m_arrow3 = (UISprite)typeof(CityServiceWorldInfoPanel).GetField("m_arrow3", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
             var m_outputSprite = (UISprite)typeof(CityServiceWorldInfoPanel).GetField("m_outputSprite", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
             var m_ShowIndustryInfoButton = (UIButton)typeof(CityServiceWorldInfoPanel).GetField("m_ShowIndustryInfoButton", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
 
-            if (m_fishFarmAI != null)
+            AquacultureFarmAI m_aquacultureFarmAI = data.Info.GetAI() as AquacultureFarmAI;
+
+            if (m_aquacultureFarmAI != null)
             {
-                m_outputBuffer.progressColor = IndustryWorldInfoPanel.instance.GetResourceColor(m_fishFarmAI.m_outputResource);
-                string text = Locale.Get("WAREHOUSEPANEL_RESOURCE", m_fishFarmAI.m_outputResource.ToString());
+                m_outputBuffer.progressColor = IndustryWorldInfoPanel.instance.GetResourceColor(m_aquacultureFarmAI.m_outputResource);
+                string text = Locale.Get("WAREHOUSEPANEL_RESOURCE", m_aquacultureFarmAI.m_outputResource.ToString());
                 m_outputLabel.text = text;
                 m_arrow3.tooltip = StringUtils.SafeFormat(Locale.Get("INDUSTRYBUILDING_EXTRACTINGTOOLTIP"), text);
-                m_outputSprite.spriteName = IndustryWorldInfoPanel.ResourceSpriteName(m_fishFarmAI.m_outputResource, false);
+                m_outputSprite.spriteName = IndustryWorldInfoPanel.ResourceSpriteName(m_aquacultureFarmAI.m_outputResource, false);
                 m_ShowIndustryInfoButton.isVisible = false;
             }
 
