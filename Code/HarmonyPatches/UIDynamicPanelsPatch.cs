338cd1dcb442bd6a750531904ac3c8e484f59b15
Tal <talrofeh@gmail.com>
2023-04-09 00:07:07 +0300

    catch null error

--- Code/HarmonyPatches/UIDynamicPanelsPatch.cs
+++ Code/HarmonyPatches/UIDynamicPanelsPatch.cs
@@ -20,9 +20,15 @@ namespace IndustriesMeetsSunsetHarbor.HarmonyPatches
         {
             var m_CachedPanels = (Dictionary<string, DynamicPanelInfo>)typeof(UIDynamicPanels).GetField("m_CachedPanels", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance);
             var newUniqueFactorydDynamicPanelInfo = CreateDynamicPanelInfo(__instance, view, "NewUniqueFactoryWorldInfoPanel", "UniqueFactoryWorldInfoPanel");
-            m_CachedPanels.Add(newUniqueFactorydDynamicPanelInfo.name, newUniqueFactorydDynamicPanelInfo);
+            if(newUniqueFactorydDynamicPanelInfo != null)
+            {
+                m_CachedPanels.Add(newUniqueFactorydDynamicPanelInfo.name, newUniqueFactorydDynamicPanelInfo);
+            }
             var restaurantAIdynamicPanelInfo = CreateDynamicPanelInfo(__instance, view, "RestaurantAIWorldInfoPanel", "UniqueFactoryWorldInfoPanel");
-            m_CachedPanels.Add(restaurantAIdynamicPanelInfo.name, restaurantAIdynamicPanelInfo);
+            if(restaurantAIdynamicPanelInfo != null)
+            {
+                m_CachedPanels.Add(restaurantAIdynamicPanelInfo.name, restaurantAIdynamicPanelInfo);
+            }
             typeof(UIDynamicPanels).GetField("m_CachedPanels", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(__instance, m_CachedPanels);
         }
 
@@ -30,14 +36,18 @@ namespace IndustriesMeetsSunsetHarbor.HarmonyPatches
         {
             DynamicPanelInfo dynamicPanelInfo = new();
             var customOldWorldInfoPanel = Array.Find(__instance.m_DynamicPanels, element => element.name == customOldWorldInfoPanelName);
-            var customOldUIClone = UnityEngine.Object.Instantiate(customOldWorldInfoPanel.instance);
-            customOldUIClone.name = customWorldInfoPanelName;
+            if(customOldWorldInfoPanel == null)
+            {
+                return null;
+            }
+            var customOldUIClone = Object.Instantiate(customOldWorldInfoPanel.instance);
+            customOldUIClone.gameObject.name = customWorldInfoPanelName;
             typeof(DynamicPanelInfo).GetField("m_Name", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(dynamicPanelInfo, customWorldInfoPanelName);
             typeof(DynamicPanelInfo).GetField("m_PanelRoot", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(dynamicPanelInfo, customOldUIClone);
             typeof(DynamicPanelInfo).GetField("m_SingleInstance", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(dynamicPanelInfo, true);
             typeof(DynamicPanelInfo).GetField("m_IsModal", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(dynamicPanelInfo, false);
             dynamicPanelInfo.viewOwner = view;
-	    GameObject gameObject = UnityEngine.Object.Instantiate(dynamicPanelInfo.panelRoot.gameObject);
+	    GameObject gameObject = Object.Instantiate(dynamicPanelInfo.panelRoot.gameObject);
             var old_component = gameObject.GetComponent<UniqueFactoryWorldInfoPanel>();
             Object.DestroyImmediate(old_component);
             if(customWorldInfoPanelName == "NewUniqueFactoryWorldInfoPanel")
