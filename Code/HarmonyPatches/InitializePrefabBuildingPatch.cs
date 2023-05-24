918187fbfceab412269e4270e0ab866926916364
Tal <talrofeh@gmail.com>
2023-05-24 19:11:16 +0300

    convert all warehouses and storage to the extended warehouse ai

--- Code/HarmonyPatches/InitializePrefabBuildingPatch.cs
+++ Code/HarmonyPatches/InitializePrefabBuildingPatch.cs
@@ -14,87 +14,77 @@ namespace IndustriesMeetsSunsetHarbor.HarmonyPatches
         {
             try
             {
-                if (__instance.m_class.m_service == ItemClass.Service.Fishing && __instance.name.Contains("Fish Market 01") && __instance.GetAI() is not ResourceMarketAI)
+                if (__instance.m_class.m_service == ItemClass.Service.Fishing)
                 {
-                    var oldAI = __instance.GetComponent<PrefabAI>();
-                    Object.DestroyImmediate(oldAI);
-                    var newAI = (PrefabAI)__instance.gameObject.AddComponent<ResourceMarketAI>();
-                    PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
-                }
-                else if (__instance.m_class.m_service == ItemClass.Service.Fishing && (__instance.name.Contains("Aqua Crops Extractor") || __instance.name.Contains("Aqua Fish Extractor")) && __instance.GetAI() is not AquacultureExtractorAI)
-                {
-                    var oldAI = __instance.GetComponent<PrefabAI>();
-                    Object.DestroyImmediate(oldAI);
-                    var newAI = (PrefabAI)__instance.gameObject.AddComponent<AquacultureExtractorAI>();
-                    PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
-                }
-                else if (__instance.m_class.m_service == ItemClass.Service.Fishing && __instance.name.Contains("Aquaculture") &&  __instance.name.Contains("Dock") && __instance.GetAI() is not AquacultureFarmAI)
-                {
-                    var oldAI = __instance.GetComponent<PrefabAI>();
-                    Object.DestroyImmediate(oldAI);
-                    var newAI = (PrefabAI)__instance.gameObject.AddComponent<AquacultureFarmAI>();
-                    PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
-                }
-                else if (__instance.m_class.m_service == ItemClass.Service.PlayerIndustry &&  (__instance.name.Contains("Food Factory 01") || __instance.name.Contains("Lemonade Factory 01") || __instance.name.Contains("Bakery 01")) && __instance.GetAI() is not NewUniqueFactoryAI && !__instance.name.Contains("Sub"))
-                {
-                    var oldAI = __instance.GetComponent<PrefabAI>();
-                    Object.DestroyImmediate(oldAI);
-                    var newAI = (PrefabAI)__instance.gameObject.AddComponent<NewUniqueFactoryAI>();
-                    PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
-                }
-                else if (__instance.m_class.m_service == ItemClass.Service.PlayerIndustry && __instance.m_class.name.Contains("Warehouses") && __instance.GetAI() is not ExtendedWarehouseAI && !__instance.name.Contains("Sub"))
-                {
-                    var oldAI = __instance.GetComponent<PrefabAI>();
-                    Object.DestroyImmediate(oldAI);
-                    var newAI = (PrefabAI)__instance.gameObject.AddComponent<ExtendedWarehouseAI>();
-                    PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
-                }
-                
-            }
-            catch (Exception e)
-            {
-                LogHelper.Error(e.ToString());
-            }
-        }
-
-        public static void Postfix()
-        {
-            try
-            {
-                uint index = 0U;
-                for (; PrefabCollection<BuildingInfo>.LoadedCount() > index; ++index)
-                {
-                    BuildingInfo buildingInfo = PrefabCollection<BuildingInfo>.GetLoaded(index);
-
-                    if (buildingInfo != null && buildingInfo.GetAI() is NewUniqueFactoryAI newUniqueFactoryAI)
+                    if (__instance.name.Contains("Fish Market 01") && __instance.GetAI() is not ResourceMarketAI)
                     {
-                        if(newUniqueFactoryAI.name.Contains("Food Factory 01") && newUniqueFactoryAI.m_outputResource != ExtendedTransferManager.TransferReason.FoodSupplies)
-                        {
-                            newUniqueFactoryAI.m_outputResource = ExtendedTransferManager.TransferReason.FoodSupplies;
-                        }
-                        else if((newUniqueFactoryAI.name.Contains("Lemonade Factory 01") || newUniqueFactoryAI.name.Contains("Drinks Factory 01")) &&
-                            newUniqueFactoryAI.m_outputResource != ExtendedTransferManager.TransferReason.DrinkSupplies)
-                        {
-                            newUniqueFactoryAI.m_outputResource = ExtendedTransferManager.TransferReason.DrinkSupplies;
-                            newUniqueFactoryAI.name = "Drinks Factory 01";
-                        }
-                        else if(newUniqueFactoryAI.name.Contains("Bakery 01") && newUniqueFactoryAI.m_outputResource != ExtendedTransferManager.TransferReason.Bread)
-                        {
-                            newUniqueFactoryAI.m_outputResource = ExtendedTransferManager.TransferReason.Bread;
-                        }
-                    }
-                    else if (buildingInfo != null && buildingInfo.GetAI() is WarehouseAI)
-                    {
-                        var oldAI = buildingInfo.GetComponent<WarehouseAI>();
+                        var oldAI = __instance.GetComponent<PrefabAI>();
                         Object.DestroyImmediate(oldAI);
-                        var newAI = (PrefabAI)buildingInfo.gameObject.AddComponent<ExtendedWarehouseAI>();
+                        var newAI = (PrefabAI)__instance.gameObject.AddComponent<ResourceMarketAI>();
+                        PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
+                    }
+                    else if ((__instance.name.Contains("Aqua Crops Extractor") || __instance.name.Contains("Aqua Fish Extractor")) && __instance.GetAI() is not AquacultureExtractorAI)
+                    {
+                        var oldAI = __instance.GetComponent<PrefabAI>();
+                        Object.DestroyImmediate(oldAI);
+                        var newAI = (PrefabAI)__instance.gameObject.AddComponent<AquacultureExtractorAI>();
+                        PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
+                    }
+                    else if (__instance.name.Contains("Aquaculture") &&  __instance.name.Contains("Dock") && __instance.GetAI() is not AquacultureFarmAI)
+                    {
+                        var oldAI = __instance.GetComponent<PrefabAI>();
+                        Object.DestroyImmediate(oldAI);
+                        var newAI = (PrefabAI)__instance.gameObject.AddComponent<AquacultureFarmAI>();
+                        PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
+                    }
+                }
+                else if (__instance.m_class.m_service == ItemClass.Service.PlayerIndustry)
+                {
+                    var component = __instance.GetComponent<PrefabAI>();
+                    if(component != null && component is WarehouseAI)
+                    {
+                        var oldAI = __instance.GetComponent<PrefabAI>();
+                        Object.DestroyImmediate(oldAI);
+                        var newAI = __instance.gameObject.AddComponent<ExtendedWarehouseAI>();
+                        PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
+                    }
+                    if (__instance.name.Contains("Food Factory 01") && __instance.GetAI() is not NewUniqueFactoryAI && !__instance.name.Contains("Sub"))
+                    {
+                        var oldAI = __instance.GetComponent<PrefabAI>();
+                        Object.DestroyImmediate(oldAI);
+                        var newAI = __instance.gameObject.AddComponent<NewUniqueFactoryAI>();
+                        PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
+                        newAI.m_outputResource = ExtendedTransferManager.TransferReason.FoodSupplies;
+                    }
+                    else if (__instance.name.Contains("Lemonade Factory 01") && __instance.GetAI() is not NewUniqueFactoryAI && !__instance.name.Contains("Sub"))
+                    {
+                        var oldAI = __instance.GetComponent<PrefabAI>();
+                        Object.DestroyImmediate(oldAI);
+                        var newAI = __instance.gameObject.AddComponent<NewUniqueFactoryAI>();
+                        PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
+                        newAI.m_outputResource = ExtendedTransferManager.TransferReason.DrinkSupplies;
+                        __instance.name = "Drinks Factory 01";
+                    }
+                    else if (__instance.name.Contains("Bakery 01") && __instance.GetAI() is not NewUniqueFactoryAI && !__instance.name.Contains("Sub"))
+                    {
+                        var oldAI = __instance.GetComponent<PrefabAI>();
+                        Object.DestroyImmediate(oldAI);
+                        var newAI = __instance.gameObject.AddComponent<NewUniqueFactoryAI>();
+                        PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
+                        newAI.m_outputResource = ExtendedTransferManager.TransferReason.Bread;
+                    }
+                    else if ((__instance.name.Contains("Warehouse Yard 01") || __instance.name.Contains("Small Warehouse 01") || __instance.name.Contains("Medium Warehouse 01") || __instance.name.Contains("Large Warehouse 01")) && __instance.GetAI() is not ExtendedWarehouseAI && !__instance.name.Contains("Sub"))
+                    {
+                        var oldAI = __instance.GetComponent<PrefabAI>();
+                        Object.DestroyImmediate(oldAI);
+                        var newAI = (PrefabAI)__instance.gameObject.AddComponent<ExtendedWarehouseAI>();
                         PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
                     }
                 }
             }
             catch (Exception e)
             {
-                LogHelper.Error(e.Message);
+                LogHelper.Error(e.ToString());
             }
         }
 
