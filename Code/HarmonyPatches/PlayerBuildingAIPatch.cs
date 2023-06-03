30660712ba9cf54929701276a651dd6a4e1e8f0a
Tal <talrofeh@gmail.com>
2023-06-03 18:29:04 +0300

    dont make restaurant not active if no production

---
+++ Code/HarmonyPatches/PlayerBuildingAIPatch.cs
@@ -0,0 +1,20 @@
+using HarmonyLib;
+using IndustriesMeetsSunsetHarbor.AI;
+
+namespace IndustriesMeetsSunsetHarbor.HarmonyPatches
+{
+    [HarmonyPatch(typeof(PlayerBuildingAI))]
+    public static class PlayerBuildingAIPatch
+    {
+        [HarmonyPatch(typeof(PlayerBuildingAI), "ProduceGoods")]
+        [HarmonyPrefix]
+        public static bool ProduceGoods(ushort buildingID, ref Building buildingData, ref Building.Frame frameData, int productionRate, int finalProductionRate, ref Citizen.BehaviourData behaviour, int aliveWorkerCount, int totalWorkerCount, int workPlaceCount, int aliveVisitorCount, int totalVisitorCount, int visitPlaceCount)
+        {
+            if(buildingData.Info.GetAI() is RestaurantAI)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
