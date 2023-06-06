adde9c9ec5263972f32cd93f004f89659021b919
Tal <talrofeh@gmail.com>
2023-06-06 23:13:56 +0300

    fix not active bug

--- Code/HarmonyPatches/PlayerBuildingAIPatch.cs
+++ Code/HarmonyPatches/PlayerBuildingAIPatch.cs
@@ -10,7 +10,7 @@ namespace IndustriesMeetsSunsetHarbor.HarmonyPatches
         [HarmonyPrefix]
         public static bool ProduceGoods(ushort buildingID, ref Building buildingData, ref Building.Frame frameData, int productionRate, int finalProductionRate, ref Citizen.BehaviourData behaviour, int aliveWorkerCount, int totalWorkerCount, int workPlaceCount, int aliveVisitorCount, int totalVisitorCount, int visitPlaceCount)
         {
-            if(buildingData.Info.GetAI() is RestaurantAI)
+            if(buildingData.Info.GetAI() is RestaurantAI && (buildingData.m_flags & Building.Flags.Active) != 0)
             {
                 return false;
             }
