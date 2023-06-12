23cd45b78394d43b5e5abb995dc69f701b1ea516
Tal <talrofeh@gmail.com>
2023-06-12 03:50:49 +0300

    add patch to delivery person and vehicle

--- Code/HarmonyPatches/CityServiceVehicleWorldInfoPanelPatch.cs
+++ Code/HarmonyPatches/CityServiceVehicleWorldInfoPanelPatch.cs
@@ -3,6 +3,7 @@ using HarmonyLib;
 using ColossalFramework;
 using UnityEngine;
 using IndustriesMeetsSunsetHarbor.Utils;
+using IndustriesMeetsSunsetHarbor.AI;
 using System.Reflection;
 
 namespace IndustriesMeetsSunsetHarbor.HarmonyPatches
@@ -35,7 +36,7 @@ namespace IndustriesMeetsSunsetHarbor.HarmonyPatches
             }
             instanceID.Vehicle = firstVehicle;
             VehicleInfo info = Singleton<VehicleManager>.instance.m_vehicles.m_buffer[firstVehicle].Info;
-            if(info.GetService() == (ItemClass.Service)28)
+            if (info.GetService() == (ItemClass.Service)28)
             {
                 ___m_VehicleType.atlas = TextureUtils.GetAtlas("RestaurantInfoIconButtonAtlas");
                 ___m_VehicleType.spriteName = "InfoIconRestaurant";
@@ -136,5 +137,67 @@ namespace IndustriesMeetsSunsetHarbor.HarmonyPatches
             }
             return result;
         }
+    }
+
+    [HarmonyPatch(typeof(CityServiceVehicleWorldInfoPanel))]
+    public static class CitizenVehicleWorldInfoPanelPatch
+    {
+        private delegate string VehicleWorldInfoPanelUpdateBindingsDelegate(VehicleWorldInfoPanel instance);
+        private static VehicleWorldInfoPanelUpdateBindingsDelegate BaseUpdateBindings = AccessTools.MethodDelegate<VehicleWorldInfoPanelUpdateBindingsDelegate>(typeof(VehicleWorldInfoPanel).GetMethod("UpdateBindings", BindingFlags.Instance | BindingFlags.NonPublic), null, false);
+
+        [HarmonyPatch(typeof(CitizenVehicleWorldInfoPanel), "GetVehicleTypeIcon")]
+        [HarmonyPrefix]
+        public static bool UpdateBindings(CitizenVehicleWorldInfoPanel __instance, ref InstanceID ___m_InstanceID, ref UISprite ___m_VehicleType)
+        {
+            BaseUpdateBindings(__instance);
+            if (___m_InstanceID.Type == InstanceType.Vehicle && ___m_InstanceID.Vehicle != 0)
+            {
+                InstanceID instanceID = ___m_InstanceID;
+                ushort vehicle = ___m_InstanceID.Vehicle;
+                ushort firstVehicle = Singleton<VehicleManager>.instance.m_vehicles.m_buffer[vehicle].GetFirstVehicle(vehicle);
+                if (firstVehicle != 0)
+                {
+                    instanceID.Vehicle = firstVehicle;
+                    VehicleInfo info = Singleton<VehicleManager>.instance.m_vehicles.m_buffer[firstVehicle].Info;
+                    if(info.GetAI() is RestaurantDeliveryVehicleAI)
+                    {
+                        ___m_VehicleType.atlas = TextureUtils.GetAtlas("RestaurantInfoIconButtonAtlas");
+                        ___m_VehicleType.spriteName = "InfoIconRestaurant";
+                    }
+                    else
+                    {
+                        ___m_VehicleType.atlas = info.m_Atlas;
+                        ___m_VehicleType.spriteName = GetVehicleTypeIcon(info.m_vehicleType);
+                    }
+                }
+            }
+            else if (___m_InstanceID.Type == InstanceType.ParkedVehicle && ___m_InstanceID.ParkedVehicle != 0)
+            {
+                VehicleParked vehicleParked = Singleton<VehicleManager>.instance.m_parkedVehicles.m_buffer[___m_InstanceID.ParkedVehicle];
+                if(vehicleParked.Info.GetAI() is RestaurantDeliveryVehicleAI)
+                {
+                    ___m_VehicleType.atlas = TextureUtils.GetAtlas("RestaurantInfoIconButtonAtlas");
+                    ___m_VehicleType.spriteName = "InfoIconRestaurant";
+                }
+                else
+                {
+                    ___m_VehicleType.atlas = vehicleParked.Info.m_Atlas;
+                    ___m_VehicleType.spriteName = GetVehicleTypeIcon(vehicleParked.Info.m_vehicleType);
+                }
+            }
+            return false;
+        }
+
+        private static string GetVehicleTypeIcon(VehicleInfo.VehicleType type)
+	{
+	    return type switch
+	    {
+		VehicleInfo.VehicleType.Car => "IconCitizenVehicle", 
+		VehicleInfo.VehicleType.Bicycle => "IconCitizenBicycleVehicle", 
+		_ => "IconCitizenVehicle", 
+	    };
+	}
+
+
     }
 }
