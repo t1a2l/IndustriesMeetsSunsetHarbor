fc82d0c7eba9ce6703ad1d50d94c845580fbbd1d
Tal <talrofeh@gmail.com>
2024-06-01 03:14:06 +0300

    restaurant support

--- Code/HarmonyPatches/CommonBuildingAIPatch.cs
+++ Code/HarmonyPatches/CommonBuildingAIPatch.cs
@@ -9,28 +9,24 @@ namespace IndustriesMeetsSunsetHarbor.HarmonyPatches
     [HarmonyPatch(typeof(CommonBuildingAI))]
     public static class CommonBuildingAIPatch
     {
-
         [HarmonyPatch(typeof(CommonBuildingAI), "GetColor")]
         [HarmonyPrefix]
         public static bool GetColor(ushort buildingID, ref Building data, InfoManager.InfoMode infoMode, InfoManager.SubInfoMode subInfoMode, ref Color __result)
         {
-            if(infoMode == (InfoManager.InfoMode)41 && data.Info.GetAI() is not RestaurantAI)
+            if (infoMode == (InfoManager.InfoMode)41 && data.Info.GetAI() is not RestaurantAI)
             {
                 var waiting_delivery = RestaurantManager.IsBuildingWaitingForDelivery(buildingID);
                 if (waiting_delivery)
-		{
-		    __result = Singleton<InfoManager>.instance.m_properties.m_modeProperties[(int)infoMode].m_targetColor;
-		}
+                {
+                    __result = Singleton<InfoManager>.instance.m_properties.m_modeProperties[(int)infoMode].m_targetColor;
+                }
                 else
                 {
                     __result = Singleton<InfoManager>.instance.m_properties.m_neutralColor;
                 }
-		return false;
+                return false;
             }
-	    return true;
+            return true;
         }
-
-
-
     }
 }
