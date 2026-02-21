c0736b92b50f4232b5b3d121f295393ec83b1cb4
Tal <talrofeh@gmail.com>
2026-02-21 19:11:38 +0200

    fix

--- Utils/ReversePatches.cs
+++ Utils/ReversePatches.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 
 namespace IndustriesMeetsSunsetHarbor.Utils
 {
+    [HarmonyPatch]
     public static class ReversePatches
     {
         [HarmonyReversePatch]
         [HarmonyPatch(typeof(PlayerBuildingAI), "ProduceGoods")]
         [MethodImpl(MethodImplOptions.NoInlining)]
-        public static void BaseProduceGoods(object instance, ushort buildingID, ref Building buildingData, ref Building.Frame frameData, int productionRate, int finalProductionRate, ref Citizen.BehaviourData behaviour, int aliveWorkerCount, int totalWorkerCount, int workPlaceCount, int aliveVisitorCount, int totalVisitorCount, int visitPlaceCount)
+        public static void BaseProduceGoods(PlayerBuildingAI instance, ushort buildingID, ref Building buildingData, ref Building.Frame frameData, int productionRate, int finalProductionRate, ref Citizen.BehaviourData behaviour, int aliveWorkerCount, int totalWorkerCount, int workPlaceCount, int aliveVisitorCount, int totalVisitorCount, int visitPlaceCount)
         {
             string message = "ProduceGoods reverse Harmony patch wasn't applied";
             Debug.LogError(message);
@@ -20,7 +21,7 @@ namespace IndustriesMeetsSunsetHarbor.Utils
         [HarmonyReversePatch]
         [HarmonyPatch(typeof(ProcessingFacilityAI), "GetInputBufferSize1", [typeof(DistrictPolicies.Park), typeof(int)], [ArgumentType.Normal, ArgumentType.Normal])]
         [MethodImpl(MethodImplOptions.NoInlining)]
-        public static int GetInputBufferSize1(object instance, DistrictPolicies.Park policies, int storageDelta)
+        public static int GetInputBufferSize1(ProcessingFacilityAI instance, DistrictPolicies.Park policies, int storageDelta)
         {
             string message = "GetInputBufferSize1 reverse Harmony patch wasn't applied";
             Debug.LogError(message);
@@ -30,7 +31,7 @@ namespace IndustriesMeetsSunsetHarbor.Utils
         [HarmonyReversePatch]
         [HarmonyPatch(typeof(ProcessingFacilityAI), "GetInputBufferSize2", [typeof(DistrictPolicies.Park), typeof(int)], [ArgumentType.Normal, ArgumentType.Normal])]
         [MethodImpl(MethodImplOptions.NoInlining)]
-        public static int GetInputBufferSize2(object instance, DistrictPolicies.Park policies, int storageDelta)
+        public static int GetInputBufferSize2(ProcessingFacilityAI instance, DistrictPolicies.Park policies, int storageDelta)
         {
             string message = "GetInputBufferSize2 reverse Harmony patch wasn't applied";
             Debug.LogError(message);
@@ -40,7 +41,7 @@ namespace IndustriesMeetsSunsetHarbor.Utils
         [HarmonyReversePatch]
         [HarmonyPatch(typeof(ProcessingFacilityAI), "GetInputBufferSize3", [typeof(DistrictPolicies.Park), typeof(int)], [ArgumentType.Normal, ArgumentType.Normal])]
         [MethodImpl(MethodImplOptions.NoInlining)]
-        public static int GetInputBufferSize3(object instance, DistrictPolicies.Park policies, int storageDelta)
+        public static int GetInputBufferSize3(ProcessingFacilityAI instance, DistrictPolicies.Park policies, int storageDelta)
         {
             string message = "GetInputBufferSize3 reverse Harmony patch wasn't applied";
             Debug.LogError(message);
@@ -50,7 +51,7 @@ namespace IndustriesMeetsSunsetHarbor.Utils
         [HarmonyReversePatch]
         [HarmonyPatch(typeof(ProcessingFacilityAI), "GetInputBufferSize4", [typeof(DistrictPolicies.Park), typeof(int)], [ArgumentType.Normal, ArgumentType.Normal])]
         [MethodImpl(MethodImplOptions.NoInlining)]
-        public static int GetInputBufferSize4(object instance, DistrictPolicies.Park policies, int storageDelta)
+        public static int GetInputBufferSize4(ProcessingFacilityAI instance, DistrictPolicies.Park policies, int storageDelta)
         {
             string message = "GetInputBufferSize4 reverse Harmony patch wasn't applied";
             Debug.LogError(message);
@@ -58,19 +59,9 @@ namespace IndustriesMeetsSunsetHarbor.Utils
         }
 
         [HarmonyReversePatch]
-        [HarmonyPatch(typeof(ExtractingFacilityAI), "GetResourceBufferSize", [typeof(DistrictPolicies.Park), typeof(int)], [ArgumentType.Normal, ArgumentType.Normal])]
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        public static int GetResourceBufferSize(object instance, DistrictPolicies.Park policies, int storageDelta)
-        {
-            string message = "GetResourceBufferSize reverse Harmony patch wasn't applied";
-            Debug.LogError(message);
-            throw new NotImplementedException(message);
-        }
-
-        [HarmonyReversePatch]
         [HarmonyPatch(typeof(ProcessingFacilityAI), "GetOutputBufferSize", [typeof(DistrictPolicies.Park), typeof(int)], [ArgumentType.Normal, ArgumentType.Normal])]
         [MethodImpl(MethodImplOptions.NoInlining)]
-        public static int GetOutputBufferSize(object instance, DistrictPolicies.Park policies, int storageDelta)
+        public static int GetOutputBufferSize1(ProcessingFacilityAI instance, DistrictPolicies.Park policies, int storageDelta)
         {
             string message = "GetOutputBufferSize reverse Harmony patch wasn't applied";
             Debug.LogError(message);
@@ -78,9 +69,29 @@ namespace IndustriesMeetsSunsetHarbor.Utils
         }
 
         [HarmonyReversePatch]
+        [HarmonyPatch(typeof(ExtractingFacilityAI), "GetOutputBufferSize", [typeof(DistrictPolicies.Park), typeof(int)], [ArgumentType.Normal, ArgumentType.Normal])]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static int GetOutputBufferSize2(ExtractingFacilityAI instance, DistrictPolicies.Park policies, int storageDelta)
+        {
+            string message = "GetOutputBufferSize reverse Harmony patch wasn't applied";
+            Debug.LogError(message);
+            throw new NotImplementedException(message);
+        }
+
+        [HarmonyReversePatch]
+        [HarmonyPatch(typeof(ExtractingFacilityAI), "GetResourceBufferSize", [typeof(DistrictPolicies.Park), typeof(int)], [ArgumentType.Normal, ArgumentType.Normal])]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static int GetResourceBufferSize(ExtractingFacilityAI instance, DistrictPolicies.Park policies, int storageDelta)
+        {
+            string message = "GetResourceBufferSize reverse Harmony patch wasn't applied";
+            Debug.LogError(message);
+            throw new NotImplementedException(message);
+        }
+
+        [HarmonyReversePatch]
         [HarmonyPatch(typeof(CommonBuildingAI), "HandleDead")]
         [MethodImpl(MethodImplOptions.NoInlining)]
-        public static void HandleDead(object instance, ushort buildingID, ref Building buildingData, ref Citizen.BehaviourData behaviour, int citizenCount)
+        public static void HandleDead(CommonBuildingAI instance, ushort buildingID, ref Building buildingData, ref Citizen.BehaviourData behaviour, int citizenCount)
         {
             string message = "HandleDead reverse Harmony patch wasn't applied";
             Debug.LogError(message);
@@ -88,19 +99,9 @@ namespace IndustriesMeetsSunsetHarbor.Utils
         }
 
         [HarmonyReversePatch]
-        [HarmonyPatch(typeof(IndustryBuildingAI), "HandleDead2")]
+        [HarmonyPatch(typeof(ProcessingFacilityAI), "IsRawMaterial")]
         [MethodImpl(MethodImplOptions.NoInlining)]
-        public static void HandleDead2(object instance, ushort buildingID, ref Building buildingData, ref Citizen.BehaviourData behaviour, int citizenCount)
-        {
-            string message = "HandleDead2 reverse Harmony patch wasn't applied";
-            Debug.LogError(message);
-            throw new NotImplementedException(message);
-        }
-
-        [HarmonyReversePatch]
-        [HarmonyPatch(typeof(CommonBuildingAI), "IsRawMaterial")]
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        public static bool IsRawMaterial(object instance, TransferManager.TransferReason material)
+        public static bool IsRawMaterial(ProcessingFacilityAI instance, TransferManager.TransferReason material)
         {
             string message = "IsRawMaterial reverse Harmony patch wasn't applied";
             Debug.LogError(message);
@@ -110,7 +111,7 @@ namespace IndustriesMeetsSunsetHarbor.Utils
         [HarmonyReversePatch]
         [HarmonyPatch(typeof(WarehouseAI), "GetMaxLoadSize")]
         [MethodImpl(MethodImplOptions.NoInlining)]
-        public static int GetMaxLoadSize(object instance)
+        public static int GetMaxLoadSize(WarehouseAI instance)
         {
             string message = "GetMaxLoadSize reverse Harmony patch wasn't applied";
             Debug.LogError(message);
@@ -118,11 +119,31 @@ namespace IndustriesMeetsSunsetHarbor.Utils
         }
 
         [HarmonyReversePatch]
-        [HarmonyPatch(typeof(WarehouseAI), "SetContentFlags")]
+        [HarmonyPatch(typeof(WarehouseAI), "SetContentFlags", MethodType.Normal)]
         [MethodImpl(MethodImplOptions.NoInlining)]
-        public static int SetContentFlags(object instance, ushort buildingID, ref Building data, TransferManager.TransferReason material)
+        public static void SetContentFlags(WarehouseAI instance, ushort buildingID, ref Building data, TransferManager.TransferReason material)
         {
             string message = "SetContentFlags reverse Harmony patch wasn't applied";
+            Debug.LogError(message);
+            throw new NotImplementedException(message);
+        }
+
+        [HarmonyReversePatch]
+        [HarmonyPatch(typeof(IndustryBuildingAI), "GetIndustryArea")]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static byte GetIndustryArea(IndustryBuildingAI instance, ushort buildingID)
+        {
+            string message = "GetIndustryArea reverse Harmony patch wasn't applied";
+            Debug.LogError(message);
+            throw new NotImplementedException(message);
+        }
+
+        [HarmonyReversePatch]
+        [HarmonyPatch(typeof(IndustryBuildingAI), "HandleDead2", MethodType.Normal)]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void HandleDead2(IndustryBuildingAI instance, ushort buildingID, ref Building buildingData, ref Citizen.BehaviourData behaviour, int citizenCount)
+        {
+            string message = "HandleDead2 reverse Harmony patch wasn't applied";
             Debug.LogError(message);
             throw new NotImplementedException(message);
         }
